9d5ca96fb3a7db5e5420ade8af163005d6b43c5d
RockChinQ <1010553892@qq.com>
2024-01-31 16:16:02 +0800

    feat: 添加请求时输出

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use('/', createProxyMiddleware({
   target: 'https://api.openai.com',
   changeOrigin: true,
   onProxyReq: (proxyReq, req, res) => {
+    console.log('proxyReq', proxyReq);
     // 移除 'x-forwarded-for' 和 'x-real-ip' 头，以确保不传递原始客户端 IP 地址等信息
     proxyReq.removeHeader('x-forwarded-for');
     proxyReq.removeHeader('x-real-ip');
